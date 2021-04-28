94b462aa44df48b1ac7fda300e067cc3f664607f
Marcelo Silva <you@example.com>
2021-04-28 17:44:03 -0300

    adjusts

--- docker-and-github/initial-setup.sh
+++ docker-and-github/initial-setup.sh
@@ -4,7 +4,7 @@ echo
 echo "This is automatically run when the scenario starts"
 echo
 echo "Clear docker images before start lab"
-docker rmi -f $(docker image ls -q)
+docker rmi -f $(docker image ls -q) > /dev/null
 echo
 echo "Initial Setup OK"
 echo
