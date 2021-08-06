ab5cc4db90e58ea54639a2867ccf117a88182229
Marcelo Silva <you@example.com>
2021-08-06 15:32:57 -0300

    ajustes e testes

--- docker-and-swarm/initial-setup.sh
+++ docker-and-swarm/initial-setup.sh
@@ -3,8 +3,6 @@ sleep 1
 echo
 echo "This is automatically run when the scenario starts"
 echo
-# echo "Clearing docker images before start lab"
-# docker rmi -f $(docker image ls -q) > /dev/null 2> /dev/null
 hostname master
 PS1="$(hostname) $"
 echo
