fb4fa3295dba0da5523806c85356b415c17fef87
Marcelo Silva <you@example.com>
2021-08-10 01:59:07 -0300

    adjust

--- docker-and-swarm/background.sh
+++ docker-and-swarm/background.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-sudo grep -i "done" /opt/katacoda-background-finished &> /dev/null
-if [[ "$?" -eq 0 ]]; then
-exit
-fi
-while true; do sudo grep -i "done" /opt/katacoda-finished > /dev/null && break || sleep 2; done
+sleep 5
+echo "done" | sudo tee /root/katacoda-finished
 echo "Everything ready..."
-
-if [ ! -f /usr/local/bin/wait.sh ] ; then
+if [ -f /usr/local/bin/deploy.sh ] ; then
   echo "Finalise the deployment..."
   /usr/local/bin/deploy.sh
 else
   echo "No deployment..."
 fi
 echo "done" | sudo tee /root/katacoda-background-finished
+
