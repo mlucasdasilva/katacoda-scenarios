2ff6e55253e4c24fc8ea46db48141ebe6b277bc3
Marcelo Silva <you@example.com>
2021-08-10 02:51:06 -0300

    adjust

--- docker-and-swarm/foreground.sh
+++ docker-and-swarm/foreground.sh
@@ -1,1 +1,1 @@
-while [[ $(hostname) == "host01" ]] && [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done && /usr/local/bin/wait.sh
+if [[ $(hostname) == "host01" ]] ; then ; while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; /usr/local/bin/wait.sh ; fi
